b2f8eb9707d0840f0c05b14cb49bbf97ba9d6a99
Andrei <florea.andrei20@yahoo.com>
2025-10-17 11:24:55 +0300

    Lab3

--- Lab2/LibraryBookAndBorrowerTracker/Program/obj/Debug/net9.0/Program.AssemblyInfo.cs
+++ Lab2/LibraryBookAndBorrowerTracker/Program/obj/Debug/net9.0/Program.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Program")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e746a116565f54ca9d80e065241ed744d2dcf7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+387abf11619ab03f7c9ede0c61012ea950a32727")]
 [assembly: System.Reflection.AssemblyProductAttribute("Program")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Program")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
